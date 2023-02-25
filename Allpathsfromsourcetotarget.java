dc0265db93da3e8d33fa413323660fb6ffdf119d
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2023-02-25 12:32:08 +0530

    Add files via upload

---
+++ Allpathsfromsourcetotarget.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class Allpathsfromsourcetotarget {
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]) {
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 3));
+
+        graph[2].add(new Edge(2, 3));
+
+        graph[3].add(new Edge(3, 1));
+
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
+
+    }
+
+    public static void calcIndeg(ArrayList<Edge> graph[], int indeg[]) {
+        for (int i = 0; i < graph.length; i++) {
+            int v = i;
+            for (int j = 0; j < graph[v].size(); j++) {
+                Edge e = graph[v].get(j);
+                indeg[e.dest]++;
+            }
+        }
+    }
+
+    public static void printallpath(ArrayList<Edge>[] graph,int src,int dest,String path) {
+        if(src == dest){
+            System.out.println(path+dest);
+            return;
+        }
+        for (int i = 0; i < graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            printallpath(graph,e.dest,dest,path+src);
+        
+        }
+    }
+
+    public static void main(String args[]) {
+
+        int v = 6;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+        createGraph(graph);
+        int src = 5, dest = 1;
+        printallpath(graph,src,dest,"");
+    }
+}
